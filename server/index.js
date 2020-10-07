90660d5785f86cc24d80c5376f131459595ef45e
Javier Flores <javier.floresapodaca@gmail.com>
2020-10-07 16:00:00 -0700

    Proxy server up and runnig

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+
+const app = express();
+
+app.use("/:locationId", express.static(__dirname + '/../client'));
+
+app.get('/reviews/initial/:locationId', (req, res) => {
+  axios.get(`http://localhost:1010/reviews/initial/${req.params.locationId}`)
+  .then(result => {
+    console.log('success');
+    res.status(200).send(result.data);
+  })
+  .catch ( err => res.status(404).send('got results'));
+})
+
+app.get('/mosaic/:locationId', (req, res) => {
+  axios.get(`http://localhost:1000/mosaic/${req.params.locationId}`)
+  .then(result => {
+    console.log('success');
+    res.status(200).send(result.data);
+  })
+  .catch ( err => res.status(404).send('got results'));
+})
+
+app.get('/book/:locationId', (req, res) => {
+  axios.get(`http://localhost:1020/book/${req.params.locationId}`)
+  .then(result => {
+    console.log('success');
+    res.status(200).send(result.data);
+  })
+  .catch ( err => res.status(404).send('got results'));
+})
+
+app.listen(1030, () => {
+  console.log("...listening on port 1030")
+})
